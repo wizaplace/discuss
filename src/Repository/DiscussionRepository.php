e1891c1ce8c3ff629f018965bed487874fafa91b
Benoit Viguier <benoit.viguier@wizacha.com>
2015-01-22 11:45:22 +0100

    Added DiscussionRepository::getByUser

--- src/Repository/DiscussionRepository.php
+++ src/Repository/DiscussionRepository.php
@@ -9,6 +9,7 @@ namespace Wizacha\Discuss\Repository;
 
 
 use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use Wizacha\Discuss\Entity\Discussion;
 use Wizacha\Discuss\Entity\DiscussionInterface;
 
@@ -33,7 +34,7 @@ class DiscussionRepository
      */
     public function __construct(EntityManager $entityManager)
     {
-        $this->_em         = $entityManager;
+        $this->_em   = $entityManager;
         $this->_repo = $entityManager->getRepository('\Wizacha\Discuss\Entity\Discussion');
     }
 
@@ -59,5 +60,43 @@ class DiscussionRepository
         $this->_em->persist($discussion);
         $this->_em->flush();
         return $discussion->getId();
+    }
+
+    /**
+     * Get displayed discussions for a specific user
+     *
+     * @param integer $user_id
+     * @param integer $nb_per_page
+     * @param integer $page Page index starting at 0
+     * @return \Countable,\Traversable
+     */
+    public function getByUser($user_id, $nb_per_page = null, $page = null)
+    {
+        $qb   = $this->_repo->createQueryBuilder('d');
+        $expr = $qb->expr();
+        $qb->where(
+            $expr->orX(
+                $expr->andX(
+                    $expr->eq('d.initiator', ':user_id'),
+                    $expr->eq('d.status_initiator', ':status')
+                ),
+                $expr->andX(
+                    $expr->eq('d.recipient', ':user_id'),
+                    $expr->eq('d.status_recipient', ':status')
+                )
+            )
+        )->setParameters([
+            'user_id' => $user_id,
+            'status'=> Discussion\Status::DISPLAYED
+        ]);
+
+        if ($nb_per_page > 0) {
+            $qb->setMaxResults($nb_per_page);
+            if ($page > 0) {
+                $qb->setFirstResult($page * $nb_per_page);
+            }
+        }
+
+        return new Paginator($qb);
     }
 }
--- tests/Discuss/Repository/DiscussionRepository.php
+++ tests/Discuss/Repository/DiscussionRepository.php
@@ -35,4 +35,85 @@ class DiscussionRepository extends RepositoryTest
 
         $this->testEntityData($discu, $discu_data);
     }
+
+    public function test_getByUser_FilterSucceed()
+    {
+        $repo = (new Client())->getDiscussionRepository();
+
+        $user_id      = 1;
+        $expected_ids = [];
+        foreach ([0, 1, 2] as $initiator_id) {
+            $recipient_id = $initiator_id + 1;
+            foreach ([false, true] as $initiator_hidden) {
+                foreach ([false, true] as $recipient_hidden) {
+                    $discu      = $repo->get();
+                    $discu_data = [
+                        'Initiator' => $initiator_id,
+                        'Recipient' => $recipient_id,
+                    ];
+                    $this->fillEntity($discu, $discu_data);
+                    if($initiator_hidden) {
+                        $discu->hideDiscussion($initiator_id);
+                    }
+                    if($recipient_hidden) {
+                        $discu->hideDiscussion($recipient_id);
+                    }
+                    $id = $repo->save($discu);
+
+                    if(
+                        ($user_id == $recipient_id && !$recipient_hidden)
+                        || ($user_id == $initiator_id && !$initiator_hidden)
+                    ) {
+                        $expected_ids[] = $id;
+                    }
+                }
+            }
+        }
+
+        foreach($repo->getByUser($user_id) as $d) {
+            $result[] = $d->getId();
+        }
+        sort($result);
+        $this->array($result)->isIdenticalTo($expected_ids);
+    }
+
+    public function test_getByUser_PaginationSucceed()
+    {
+        $repo = (new Client())->getDiscussionRepository();
+
+        for($i=0;$i<10;++$i) {
+            $d = $repo->get();
+            $this->fillEntity($d, ['Initiator' => 1, 'Recipient' => 1]);
+            $repo->save($d);
+        }
+
+        $pager = $repo->getByUser(1);
+        $this->object($pager)->isInstanceOf('\Countable')->isInstanceOf('\Traversable');
+        //Retrieve All
+        $this->object($pager)
+            ->hasSize(10)
+            ->array(iterator_to_array($pager))->hasSize(10)
+        ;
+
+        //Retrieve complete page
+        $pager = $repo->getByUser(1, 7);
+        $this->object($pager)
+            ->hasSize(10)
+            ->array(iterator_to_array($pager))->hasSize(7)
+        ;
+
+        //Retrieve incomplete page
+        $pager = $repo->getByUser(1, 7, 1);
+        $this->object($pager)
+            ->hasSize(10)
+            ->array(iterator_to_array($pager))->hasSize(3)
+        ;
+
+        //Retrieve empty page
+        $pager = $repo->getByUser(1, 7, 2);
+        $this->object($pager)
+            ->hasSize(10)
+            ->array(iterator_to_array($pager))->hasSize(0)
+        ;
+    }
 }
